bd28d32f29d15ef761cc5f7a06ba68216b46ccbb
sofyao4 <113201319+sofyao4@users.noreply.github.com>
2023-02-13 13:15:00 -0500

    2.13

--- create-task/main.js
+++ create-task/main.js
@@ -1,4 +1,27 @@
 import "./style.css";
-const words = [(cat1 = []), (cat2 = []), (cat3 = [])];
-
-document.getElementById("category1");
+const words = [
+  "hangman",
+  "javascript",
+  "absurd",
+  "avenue",
+  "jazz",
+  "microwave",
+  "oxygen",
+  "pneumonia",
+  "syndrome",
+  "synonym",
+  "topaz",
+  "jackpot",
+  "unknown",
+  "witchcraft",
+  "xylophone",
+  "zodiac",
+];
+const history = [];
+const word = words[Math.floor(Math.random() * words.length)];
+console.log(word);
+const answerArray = [];
+for (var i = 0; i < word.length; i++) {
+  answerArray[i] = "_";
+}
+console.log(answerArray);
