9262ee781a58d6b43f401aaeb2327b5a76ea6fcf
sofyao4 <113201319+sofyao4@users.noreply.github.com>
2023-02-14 13:13:51 -0500

    2/14

--- create-task/main.js
+++ create-task/main.js
@@ -25,3 +25,28 @@ for (var i = 0; i < word.length; i++) {
   answerArray[i] = "_";
 }
 console.log(answerArray);
+
+const remainingLetters = word.length;
+console.log(remainingLetters);
+document
+  .querySelector(`.gameSpace`)
+  .insertAdjacentHTML("afterend", `<div> word: ${answerArray.join(" ")}</div>`);
+
+function game() {
+  const form = document.querySelector(`.form`);
+  const input = document.querySelector(`.input`);
+  if (form > 1) {
+    alert("please insert one letter");
+  }
+}
+while (remainingLetters < 0) {
+  const guess = prompt("enter letter");
+  for (var j = 0; j < word.length; j++) {
+    if ((word[j] = guess)) {
+      answerArray[j] = guess;
+      remainingLetters--;
+    }
+    history.push(guess);
+  }
+}
+console.log(history);
