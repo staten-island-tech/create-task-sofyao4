ade3cf6a17bcf2531e43cc9bf25606383687e1d2
sofyao4 <113201319+sofyao4@users.noreply.github.com>
2023-02-27 13:14:08 -0500

    2/27

--- create-task/main.js
+++ create-task/main.js
@@ -22,14 +22,9 @@ const chooseWord = words[Math.floor(Math.random() * words.length)];
 const word = chooseWord.split("");
 console.log(chooseWord);
 console.log(word);
-const answerArray = [];
-for (var i = 0; i < word.length; i++) {
-  word[i] = "_";
-}
-console.log(word);
 
 let remainingLetters = word.length;
-console.log(remainingLetters);
+
 document
   .querySelector(`.gameSpace`)
   .insertAdjacentHTML("afterend", `<div> word: ${word.join(" ")} </div>`);
@@ -38,23 +33,21 @@ const form = document.getElementById("form");
 
 form.addEventListener("submit", function (e) {
   let guesses = document.querySelector(`#lett`).value;
-
   e.preventDefault();
   console.log(guesses);
-  // while (guesses.length <= 1) {
-  //   if (word.includes(guesses)) {
-  //     console.log("includes");
-  //   } else {
-  //     console.log("does not include");
-  //   }
-  //   history.push(guesses);
-  //   guesses += 1;
-  //   console.log(guesses);
-  //   console.log(history);
-  // }
   if (guesses.length <= 1) {
     if (word.includes(guesses)) {
       const letter = console.log();
     }
   }
 });
+const indices = [];
+const array = chooseWord.split("");
+const element = document.querySelector(`#lett`).value;
+let idx = array.indexOf(element);
+while (idx !== -1) {
+  indices.push(idx);
+  idx = array.indexOf(element, idx + 1);
+}
+console.log(indices);
+console.log(array);
